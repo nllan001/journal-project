89b1afa3909234a7036536326c1a4f4cdf7cfd39
Noel Llanura <nllan001@ucr.edu>
2014-12-04 09:09:05 -0800

    Initializing project files

---
+++ nllan001_journal_leader/nllan001_journal_leader/nllan001_journal_leader.c
@@ -0,0 +1,17 @@
+/*
+ * nllan001_journal_leader.c
+ *
+ * Created: 12/4/2014 8:36:25 AM
+ *  Author: Gerard
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
